3efa01d0cabfd8869a8db0f856e5153fa8263212
lolkindom <69848432+lolkindom@users.noreply.github.com>
2021-03-23 23:14:31 +0300

    Update for HH 1.py

--- for HH 1.py
+++ for HH 1.py
@@ -1,13 +1,7 @@
-def is_even1(value):
+def is_even(value):
     return not value & 1
 
 
-def is_even2(value):
-    return value % 10 in [0, 2, 4, 6, 8]
-
-
 if __name__ == '__main__':
-    assert is_even1(3) == False
-    assert is_even1(4) == True
-    assert is_even2(5) == False
-    assert is_even2(6) == True
+    assert is_even(3) == False
+    assert is_even(4) == True
